3116baab793051291aee993d46a36ac765c3f0eb
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-02-03 10:27:54 +0530

    Time: 3 ms (11.34%), Space: 20.8 MB (6.03%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -24,3 +24,23 @@ public:
         return head;
     }
 };
+// ListNode* removeElements(ListNode* head, int val) {
+//     ListNode* dummy = new ListNode(0);
+//     dummy->next = head;
+
+//     ListNode* curr = dummy;
+
+//     while (curr->next != NULL) {
+//         if (curr->next->val == val) {
+//             ListNode* temp = curr->next;
+//             curr->next = temp->next;
+//             delete temp;
+//         } else {
+//             curr = curr->next;
+//         }
+//     }
+
+//     ListNode* newHead = dummy->next;
+//     delete dummy;
+//     return newHead;
+// }
